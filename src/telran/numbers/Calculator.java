43304fcae300d963dff1aa8c284c415f0d33594b
mlipov <maxim.lipov@gmail.com>
2023-06-27 23:29:42 +0300

    CW-08 Exceptions Introduction

--- src/telran/numbers/Calculator.java
+++ src/telran/numbers/Calculator.java
@@ -3,11 +3,19 @@ package telran.numbers;
 import java.util.function.BinaryOperator;
 
 public class Calculator {
-	static DoubleBinaryOperator[] operators = { (a, b) -> a + b, (a, b) -> a - b, (a, b) -> a * b, (a, b) -> a / b };
+	static DoubleBinaryOperator[] operators = { (a, b) -> a + b, (a, b) -> a - b, (a, b) -> a * b, (a, b) -> {
+		if (b == 0) {
+			throw new ArithmeticException("division on 0");
+		}
+		return a / b;
+	} };
 	static char[] operations = { '+', '-', '*', '/' };
 
 	static public double calculate(CalcData cd) {
 		int index = findIndex(cd.operation);
+		if (index < 0) {
+			throw new UnsupportedOperationException(" operation " + cd.operation);
+		}
 
 		return operators[index].apply(cd.op1, cd.op2);
 	}
